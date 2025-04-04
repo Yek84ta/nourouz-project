8bcdf54432eaedfcdf4790f93c9607998729c219
Yek84ta <ghafoori.yekta@gmail.com>
2025-04-04 10:27:53 +0330

    copy related methods

---
+++ Database.java
@@ -0,0 +1,45 @@
+package db;
+
+import db.exception.EntityNotFoundException;
+
+import java.util.ArrayList;
+
+public class Database {
+
+    private static ArrayList<Entity> entities = new ArrayList<>();
+    public static int idCreation;
+
+    private Database() {
+    }
+
+    public static void add(Entity e) {
+        entities.add(e);
+        idCreation ++;
+        e.id = idCreation;
+    }
+
+    public static Entity get(int id) {
+        for (Entity entity : entities)
+            if (entity.id == id)
+                return entity.clone(); // Return a clone instead of original
+        throw new EntityNotFoundException(id);
+    }
+
+    public static void delete(int id) {
+        for (Entity entity : entities)
+            if (entity.id == id) {
+                entities.remove(entity);
+                return;
+            }
+        throw new EntityNotFoundException(id);
+    }
+
+    public static void update(Entity e) {
+        for (Entity entity : entities)
+            if (entity.id == e.id) {
+                entities.set(entities.indexOf(entity), e.clone());
+                return;
+            }
+        throw new EntityNotFoundException();
+    }
+}
